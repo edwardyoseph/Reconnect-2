ab3e12b8d17b8c09f63611143fb393e9840819d3
edwardyoseph <116825212+edwardyoseph@users.noreply.github.com>
2025-12-19 18:24:27 +0700

    Update ImportDeltaKey.sh

--- Reconnect/ImportDeltaKey.sh
+++ Reconnect/ImportDeltaKey.sh
@@ -1,5 +1,9 @@
 echo "Current License Key:"
-cat /storage/emulated/0/Delta/Internals/Cache/license
+if [[ -f /storage/emulated/0/Delta/Internals/Cache/license ]]; then
+    cat /storage/emulated/0/Delta/Internals/Cache/license
+else
+    echo "License file not found, skipping cat."
+fi
 echo "  "
 echo "Import New Delta Key:"
 read new_key
@@ -7,5 +11,10 @@ echo "$new_key" > /storage/emulated/0/Delta/Internals/Cache/license
 echo "Delta Key berhasil diimport!"
 echo "  "
 echo "Updated License Key:"
-cat /storage/emulated/0/Delta/Internals/Cache/license
+if [[ -f /storage/emulated/0/Delta/Internals/Cache/license ]]; then
+    cat /storage/emulated/0/Delta/Internals/Cache/license
+else
+    echo "License file not found after import."
+fi
 echo "  "
+echo "Please restart Delta to apply the new key."
